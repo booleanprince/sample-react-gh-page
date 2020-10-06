a10e9b9a3edf1ae654a299a7b945812ac3506952
Rommer Canete <rscanete@vibalgroup.com>
2020-10-06 16:03:17 +0800

    Updates

---
+++ precache-manifest.c8d50cb85ed58792bd5f874d989883aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ecd71eb6e776407ccc0747db2c8fbe",
+    "url": "/sample-react-gh-page/index.html"
+  },
+  {
+    "revision": "839fdb6326c5f706dc38",
+    "url": "/sample-react-gh-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e928709eaab80c9ae254",
+    "url": "/sample-react-gh-page/static/js/2.b4c7c8b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-react-gh-page/static/js/2.b4c7c8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839fdb6326c5f706dc38",
+    "url": "/sample-react-gh-page/static/js/main.a8253186.chunk.js"
+  },
+  {
+    "revision": "1322675c5186e88145e1",
+    "url": "/sample-react-gh-page/static/js/runtime-main.b5eeed9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-react-gh-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
